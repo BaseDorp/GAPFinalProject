3b0a43739758cd47ee6d586e301f489b8b6bca99
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2019-10-27 19:37:26 -0500

    Adds Basic Entity support using DOTS

---
+++ SHFinalProject/Assets/Scripts/MoveSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class MoveSystem : ComponentSystem
+{
+    float movementSpeed = 4f;
+
+    protected override void OnUpdate()
+    {
+        // Code executes for entities that contains a translation component
+        Entities.ForEach((ref Translation translation) =>
+        {
+            translation.Value.y += movementSpeed * Time.deltaTime;
+            // Changes the entity's direction if it reaches the top or bottom of the screen
+            if (translation.Value.y > 3f)
+            {
+                movementSpeed = -math.abs(movementSpeed);
+            }
+            if (translation.Value.y < -3f)
+            {
+                movementSpeed = +math.abs(movementSpeed);
+            }
+        });
+    }
+}
---
+++ SHFinalProject/Assets/Scripts/Test.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Rendering;
+using Unity.Mathematics;
+
+public class Test : MonoBehaviour
+{
+    public int NumberofEntities = 1;
+    [SerializeField]
+    private Mesh mesh;
+    [SerializeField]
+    private Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Manager that looks over all the entities
+        EntityManager entityManager = World.Active.EntityManager;
+
+        // Attributes that the Entities have
+        EntityArchetype entityArchetype = entityManager.CreateArchetype( 
+            typeof(Translation),
+            typeof(RenderMesh),
+            typeof(LocalToWorld)
+            );
+
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(NumberofEntities, Allocator.Temp);
+        entityManager.CreateEntity(entityArchetype, entityArray);
+
+        for (int i = 0; i < entityArray.Length; i++)
+        {
+            Entity entity = entityArray[i];
+            // Spawns each entity in a random location
+            entityManager.SetComponentData(entity, new Translation { Value = new float3(UnityEngine.Random.Range(-8, 8), UnityEngine.Random.Range(-5, 5), 0) });
+
+            // Assigns the mesh and material to each entity
+            entityManager.SetSharedComponentData(entity, new RenderMesh
+            {
+                mesh = mesh,
+                material = mat,
+            });
+        }
+
+        //Empty the native array
+        entityArray.Dispose();
+    }
+}
