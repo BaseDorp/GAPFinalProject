02d7e07a71d7b42579d95058be4c7dbe653780b7
Sam Hirsch <362328@colum.edu>
2019-10-28 11:48:08 -0500

    ProcMeshDemo1
    Procedural Mesh Demo for in class Assignment

---
+++ SHFinalProject/Assets/Scripts/ProceduralCube.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Code Sourced from https://youtu.be/bnmr_At2R0s
+
+[RequireComponent (typeof(MeshFilter), typeof(MeshRenderer))]
+public class ProceduralCube : MonoBehaviour
+{
+    Mesh mesh;
+    List<Vector3> vertices;
+    List<int> triangles;
+
+    private void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    private void Start()
+    {
+        MakeCube();
+        UpdateMesh();
+    }
+
+    void MakeFace(int dir)
+    {
+        vertices.AddRange(CubeMeshData.faceVertices(dir));
+        int vCount = vertices.Count;
+
+        triangles.Add(vCount - 4);
+        triangles.Add(vCount - 4 + 1);
+        triangles.Add(vCount - 4 + 2);
+        triangles.Add(vCount - 4);
+        triangles.Add(vCount - 4 + 2);
+        triangles.Add(vCount - 4 + 3);
+    }
+
+    void MakeCube()
+    {
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            MakeFace(i);
+        }
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+    }
+}
---
+++ SHFinalProject/Assets/Scripts/ProceduralMesh.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Code sourced from https://youtu.be/v9E47DkckBE
+
+[RequireComponent(typeof(MeshFilter))]
+public class ProceduralMesh : MonoBehaviour
+{
+    Mesh mesh;
+    int x = 0;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+    private void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+    private void Start()
+    {
+        MakeMeshData();
+        CreateMesh();
+    }
+
+    private void Update()
+    {
+
+        CreateMesh();
+        if (Input.GetKey("right"))
+        {
+            x++;
+            vertices = new Vector3[] { new Vector3(x, 0, 0), new Vector3(x, 0, 1), new Vector3(x + 1, 0, 0), new Vector3(x + 1, 0, 1) };
+        }
+        if (Input.GetKey("left"))
+        {
+            x--;
+            vertices = new Vector3[] { new Vector3(x, 0, 0), new Vector3(x, 0, 1), new Vector3(x + 1, 0, 0), new Vector3(x + 1, 0, 1) };
+        }
+    }
+
+    void MakeMeshData()
+    {
+        // Array of verticies
+        vertices = new Vector3[] { new Vector3(x,0,0), new Vector3(x,0,1), new Vector3(x+1,0,0), new Vector3(x+1,0,1) };
+
+        // What order should the verticies be drawn in
+        triangles = new int[] { 0, 1, 2, 2, 1, 3 };
+    }
+
+    void CreateMesh()
+    {
+        // Clears the current mesh and then creates the new mesh
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+}
+
