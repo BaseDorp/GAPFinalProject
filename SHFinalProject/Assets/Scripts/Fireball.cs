d1c3fbb687038ec68eb28e989939d162712f2325
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2019-12-09 04:47:49 -0600

    Final Commit

---
+++ SHFinalProject/Assets/Scripts/Fireball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    Rigidbody Fireballrb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Fireballrb = GetComponent<Rigidbody>();
+        Fireballrb.AddForce(transform.forward * 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y <= -5)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- SHFinalProject/Assets/Scripts/SpawnFire.cs
+++ SHFinalProject/Assets/Scripts/SpawnFire.cs
@@ -4,22 +4,78 @@ using UnityEngine;
 
 public class SpawnFire : MonoBehaviour
 {
+    bool fireActiveRight = false;
+    bool fireActiveLeft = false;
+    int numFirballs = 0;
+
+    int maxFireballs = 1;
+
+    int i = 0;
+
     [SerializeField]
-    GameObject fire;
+    ParticleSystem FireRight;
+    [SerializeField]
+    ParticleSystem FireLeft;
+    [SerializeField]
+    GameObject FireBall;
+    [SerializeField]
+    Transform LeftHandLoc;
+    [SerializeField]
+    Transform RightHandLoc;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        FireRight.Stop();
+        FireLeft.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.Get(OVRInput.Button.One))
+        // Fire Right Hand
+        if (OVRInput.GetDown(OVRInput.Button.Two) && fireActiveRight == false)
         {
-            Debug.Log("pressed");
-            Instantiate(fire, this.transform);
+            FireRight.Play();
+            fireActiveRight = true;
+        }
+        if (OVRInput.GetDown(OVRInput.Button.One) && fireActiveRight == true)
+        {
+            FireRight.Stop();
+            fireActiveRight = false;
+        }
+
+        // Fire Left Hand
+        if (OVRInput.GetDown(OVRInput.Button.Four) && fireActiveLeft == false)
+        {
+            FireLeft.Play();
+            fireActiveLeft = true;
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Three) && fireActiveLeft == true)
+        {
+            FireLeft.Stop();
+            fireActiveLeft = false;
+        }
+
+        Debug.Log(OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch));
+
+        // Spawn fireball Right hand       
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.RTouch) >= .5f)
+        {
+            if (Time.time > i)
+            {
+                i += 1;
+                Instantiate(FireBall, RightHandLoc.position, new Quaternion(0,0,0,0));
+            }
+        }
+        //Spawn fireball Left hand
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch) >= .5f)
+        {
+            if (Time.time > i)
+            {
+                i += 1;
+                Instantiate(FireBall, LeftHandLoc.position, new Quaternion(0, 0, 0, 0));
+            }
         }
     }
 }
